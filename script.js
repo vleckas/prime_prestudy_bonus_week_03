7eeb3813e2675cd1810029509b7a8c100b7d5b03
Vas Leckas <vas.leckas@gmail.com>
2015-07-15 18:22:29 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var functionA = function (number) {
+    var val = number * 3;
+    return (val);
+};
+var functionB = function (number) {
+    var val = number * 4;
+    return (val);
+};
+var functionC = function (number) {
+    var val = number * 5;
+    return (val);
+};
